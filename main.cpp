6902d19bb999f2f40bdbce39964b09d6e8e67e12
Foridul Islam <foridulislamdise880@gmail.com>
2025-02-05 19:04:30 +0600

    C languase practice

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   string Name="Foridul islam";
+   cout << Name << endl;
+   return 0;
+}
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main() {
+//   string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+//   cout << cars[0];
+//   return 0;
+// }
